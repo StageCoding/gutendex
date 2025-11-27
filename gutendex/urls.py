f7414803e89fde155379847688ea351201260141
Enver Suljic <enver@stagecoding.com>
2025-11-27 16:13:30 +0100

    Revert "iter"
    This reverts commit 9d63112b81edeba43d33951eb95d6efa1c611fe0.

--- gutendex/urls.py
+++ gutendex/urls.py
@@ -11,6 +11,6 @@ router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='home.html')),
-    re_path(r'^libraryCategories/?$', views.LibraryCategoriesView.as_view()),
+    re_path(r'^libraryCategories$', views.LibraryCategoriesView.as_view()),
     re_path(r'^', include(router.urls)),
 ]
